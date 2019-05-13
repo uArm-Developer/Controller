e25c7f9c38cc7a37c61c1016e1240888fe8f2211
HuttonGe <geweipan@ufactory.cc>
2019-05-13 15:41:51 +0800

    update 2019-5-13

--- scene_demo/vision_scratch cube/code for Controller/controller-vision/controller-vision.ino
+++ scene_demo/vision_scratch cube/code for Controller/controller-vision/controller-vision.ino
@@ -32,9 +32,8 @@ void setup() {
   first_page();
   while (digitalRead(BUTTON_A) == HIGH);
   LED_ON;
-//  second_page();
-//  openmv_connect();
-
+  //  second_page();
+  //  openmv_connect();
   Serial.write("V2 START!\n");
 
   Serial2.write("M2400 S0\n");//set the mode of uarm
@@ -43,39 +42,30 @@ void setup() {
   Serial2.write("M2122 V1\n");//report when finish the movemnet
   //  get_openmv_data();
   times = millis();
-  cube_flag=false;
-//  pick_and_palce();
+  cube_flag = false;
 }
 void work()
 {
-  if ((flag == 0)&&(cube_flag == false))
+  if ((flag == 0) && (cube_flag == false))
   {
     LED_ON;
-
     //    mode_choose();
     Serial2.write("G0 X200 Y0 Z160 F10000\n");
-
-
-//    wait_for_finish_moving();
-       delay(2400);
-
-        Serial2.write("G0 X200 Y0 Z160 F10000\n");
-//    delay(500);//wait for the uarm to finish the moving then start the vision tracking
-//        Serial2.begin(115200);
-
-//    wait_for_finish_moving();
+    wait_for_finish_moving();
+    delay(2400);
+    Serial2.write("G0 X200 Y0 Z160 F10000\n");
+    delay(500);//wait for the uarm to finish the moving then start the vision tracking
+    Serial2.begin(115200);
+    wait_for_finish_moving();
     Serial3.begin(115200);
-    
     flag = 1;//vision start
     openmv_connect();
     times = millis();
-
   }
   LED_OFF;
   //get object coordinates from openmv
   if (get_openmv_data() == 1)
   {
-
     Serial.write("move\n");//confirm the openmv data
     Serial3.end();
     //new algorithm
@@ -97,13 +87,13 @@ void work()
     pick_and_palce();
     cube_flag = false;
     num = 0;
-        flag = 0;//vision end
+    flag = 0;//vision end
 
   }
   if ((buf[0] != 's') && (cube_flag == false))
   {
     //flag = 0;//vision end
-    num=0;
+    num = 0;
     Serial.println("jsk");
     openmv_flag = false;
     openmv_connect();
@@ -114,18 +104,9 @@ void work()
   error_page();
 }
 void loop() {
-   fifth_page();
-  // delay(3000);
-//  button_mode();
-
-  // joystick_work();
-  // x_uarm=x_offest+200;
-  // y_uarm=y_offest+0;
-  //     String commands = "G0 X";
-  //    commands.concat(x_uarm);
-  //    commands += " Y";
-  //    commands.concat(y_uarm);
-  //    commands += " Z100 F10000\n";
-  //    Serial2.print(commands);
-  //    offest_page();
+  delay(3000);
+  if (mode_choose == true)
+  {
+    work();
+  }
 }
